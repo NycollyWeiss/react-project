e1abc1e3e32f9188689b1e19ffc78f26d95e8ef5
NycollyWeiss <nycollyweissoliveira@gmail.com>
2025-04-09 14:23:57 -0300

    atualizacao das models e logica de implementacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import Home from './pages/home/Home'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/AuthContext';
+import Cadastro from './pages/cadastro/Cadastro';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+import FormTema from './components/temas/formtemas/FormTemas';
 
 function App() {
-    return (
-        <Home />
-    )
+
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
