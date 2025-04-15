1e2671039326c47982a18fc97707ac018d0c280f
NycollyWeiss <nycollyweissoliveira@gmail.com>
2025-04-15 14:00:06 -0300

    crud postagem e perfil

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,18 @@ import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import ListaTemas from './components/temas/listatemas/ListaTemas';
 import FormTema from './components/temas/formtemas/FormTemas';
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens';
+import FormPostagem from './components/postagens/formpostagem/FormPostagem';
+import DeletarPostagem from './components/temas/deletarpostagem/DeletarPostagem';
+import Perfil from './pages/perfil/Perfil';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 
   return (
     <AuthProvider>
+      <ToastContainer />
       <BrowserRouter>
         <Navbar />
         <div className="min-h-[80vh]">
@@ -24,6 +31,12 @@ function App() {
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
+              
           </Routes>
         </div>
         <Footer />
